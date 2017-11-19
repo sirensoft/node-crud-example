42ada8db295aba285c16ac11b4641da527051c8c
tehnn <tehnnn@gmail.com>
2017-11-19 09:20:54 +0700

    Sun 11/19/2017  9:20:54.09

--- controllers/apiController.js
+++ controllers/apiController.js
@@ -5,7 +5,8 @@ var Person = require('../models/person')
 var mPerson = new Person();
 var jwtCheck = require('../modules/jwtCheck')
 
-router.get('/persons', jwtCheck, async(req, res) => {
+router.use(jwtCheck);
+router.get('/persons', async(req, res) => {
     let persons = await mPerson.findAll();
     res.status(200).json(persons)
 });
